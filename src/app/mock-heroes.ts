ffcddb6248d8cbe423720b19be06de0ec291e2ce
d.pak <d.pak@bars.group>
2020-08-10 03:29:24 +0300

    Added YB debbugging + features

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const Heroes: Hero[] = [
+  { id: 11, name: 'Wolverine' },
+  { id: 12, name: 'Thor' },
+  { id: 13, name: 'Cyclops' },
+  { id: 14, name: 'Iron Man' },
+  { id: 15, name: 'Daredevil' },
+  { id: 16, name: 'Hulk' },
+  { id: 17, name: 'Silver Surfer' },
+];
